f2fd6b0655461e5c36d3dd05bc2c1d29d6990721
Nomchin Banga <bangan@purdue.edu>
2017-04-08 16:03:49 -0400

    Update lookup3.h

--- lookup3.h
+++ lookup3.h
@@ -1,5 +1,10 @@
-#include <stdint.h>
-#include <stdio.h>
+#include <stdio.h>      /* defines printf for tests */
+#include <time.h>       /* defines time_t for timings in the test */
+#include <stdint.h>     /* defines uint32_t etc */
+#include <sys/param.h>  /* attempt to define endianness */
+#ifdef linux
+# include <endian.h>    /* attempt to define endianness */
+#endif
 
 uint32_t hashlittle( const void *key, size_t length, uint32_t initval);
 void hashlittle2(const void *key, size_t length, uint32_t *pc, uint32_t *pb);
