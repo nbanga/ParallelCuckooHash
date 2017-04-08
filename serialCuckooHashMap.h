7d7754eff1d82a4459798ed0ce23d57e1f2b7721
Nomchin Banga <bangan@purdue.edu>
2017-04-08 16:14:11 -0400

    header for parallel cuckoo hashmap

--- serialCuckooHashMap.h
+++ serialCuckooHashMap.h
@@ -3,9 +3,15 @@
 #include <string.h>
 #include <stdbool.h>
 #include <limits.h>
+#include "lookup3.h"
 
 #define NUM_SLOTS 4
 #define MAX_ITERATIONS 100
+
+typedef enum{
+    hashlittle2, 
+    hashword2
+}hash_function;
 
 typedef struct node{
     char *key;
@@ -15,6 +21,9 @@ typedef struct node{
 typedef struct{
     entryNode** buckets;
     int num_buckets;
+    int num_entries;
+    float max_load_factor;
+    hash_function hashFunction;
 }hashTable;
 
 extern hashTable* hashtable;
@@ -22,6 +31,8 @@ extern hashTable* hashtable;
 extern char* get(char*);
 extern void put(char*, char*);
 extern bool removeKey(char*);
+extern float getLoadFactor();
+extern void rehash();
 extern void resize();
 extern void createHashTable(int, float);
 extern entryNode* createEntryNode(char*, char*);
